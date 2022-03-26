85b98ffb1e37b46e7c8131dfeba45f5d16636459
wallace <wallacegibbon@outlook.com>
2022-03-27 01:36:54 +0800

    QtSerialPort is being used now. The CMakeLists.txt is now ready.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,16 +1,27 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include "ui_mainwindow.h"
 #include <QString>
 #include <QFile>
 #include <QDir>
 #include <QTextStream>
 #include <QMessageBox>
 #include <QDebug>
+#include <QtSerialPort/QSerialPortInfo>
+
+void show_all_ports()
+{
+    QList<QSerialPortInfo> ports = QSerialPortInfo::availablePorts();
+    for (const QSerialPortInfo &info : ports)
+    {
+        qDebug() << "\t-- " << info.portName();
+    }
+}
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton_1, &QPushButton::clicked, this, &MainWindow::handle_btn1_click);
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::handle_btn1_click);
+    show_all_ports();
 }
 
 MainWindow::~MainWindow()
@@ -23,8 +34,8 @@ void MainWindow::handle_btn1_click()
     // QMessageBox::about(this, "test signal", "this is just a test");
     qDebug() << "button 1 got clicked.";
     QString msg("hello");
-    QFile file(QDir::home().absoluteFilePath("hello_qt.txt"));
-    if (!file.open(QIODevice::WriteOnly))
+    QFile file(QDir::temp().absoluteFilePath("hello_qt.txt"));
+    if (!file.open(QIODevice::Append))
     {
         qWarning() << "can not open file with write access";
         return;
